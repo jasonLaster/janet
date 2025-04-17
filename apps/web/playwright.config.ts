a253df9fc14162714ee32296e06be64581f0d94d
Jason Laster <jason.laster.11@gmail.com>
2025-04-17 12:50:51 -0700

    tweaks

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const usePreviewUrl = !!process.env.PLAYWRIGHT_TEST_BASE_URL;
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const baseURL = usePreviewUrl
+  ? process.env.PLAYWRIGHT_TEST_BASE_URL
+  : `http://localhost:${port}`;
+
 export default defineConfig({
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
 
   retries: process.env.CI ? 2 : 0,
@@ -24,13 +18,7 @@ export default defineConfig({
   reporter: process.env.CI ? "html" : "dot",
 
   use: {
-    headless: false,
-
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
-
-    /* Collect trace when retrying the failed test. 
-    See https://playwright.dev/docs/trace-viewer */
+    baseURL,
     trace: "on",
   },
 
@@ -46,11 +34,4 @@ export default defineConfig({
       dependencies: ["setup"],
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
