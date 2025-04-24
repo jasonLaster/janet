a03a1e54ad62f687088ac9e347903837895d9b94
Jason Laster <jason.laster.11@gmail.com>
2025-04-24 16:07:24 -0700

    Only show the sidebar when viewing the list page

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
 import { Sidebar } from "@/components/sidebar";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen bg-background">
-      <Sidebar />
-
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <main className="flex-1 overflow-auto">{children}</main>
-      </div>
-    </div>
-  );
+  return <div className="flex h-screen bg-background">{children}</div>;
 }
--- apps/web/app/(app)/page.tsx
+++ apps/web/app/(app)/page.tsx
@@ -1,9 +1,16 @@
 import { PdfList } from "@/components/pdf-list";
+import { Sidebar } from "@/components/sidebar";
 
 export default function HomePage() {
   return (
-    <div className="">
-      <PdfList />
-    </div>
+    <>
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <main className="flex-1 overflow-auto">
+          <PdfList />
+        </main>
+      </div>
+    </>
   );
 }
